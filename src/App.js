48e300c2a5890c44f9c9294190022e42184f34d1
jkder <jksder@gmail.com>
2021-03-03 13:57:39 -0500

    revamp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import _ from "lodash";
+import React, { Component } from "react";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+const API_KEY = "AIzaSyCGCuRZ5gTwxhNKxXzEcXSwRaPEu9c_Erc";
+
+// Create a new component which will produce some HTML.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.vidSearch = this.vidSearch.bind(this);
+  }
+
+  async vidSearch(term) {
+    var youtube = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+    });
+
+    var response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        key: API_KEY,
+        q: term,
+      },
+    });
+
+    var videos = response.data.items;
+
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0],
+    });
+  }
+
+  componentDidMount() {
+    this.vidSearch("doggos");
+  }
+
+  render() {
+    const vidSearch = _.debounce((term) => {
+      this.vidSearch(term);
+    }, 400);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={this.vidSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
